5b3aa88fb667a9e1d4c52c622d998ba5d37a60b4
jqiufu <joaquinqiu123456@gmail.com>
2023-06-18 22:30:58 -0700

    Updates

---
+++ precache-manifest.3fd4b2885147b58e9a8b1dae8fa339f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49e2fa5a207d135d64db5226ba51daa",
+    "url": "/To-do-List-App/index.html"
+  },
+  {
+    "revision": "8a296adaa4a86eabafc4",
+    "url": "/To-do-List-App/static/css/main.7c8f2f72.chunk.css"
+  },
+  {
+    "revision": "2f9fdcdcadd78e491363",
+    "url": "/To-do-List-App/static/js/2.9b887dbb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/To-do-List-App/static/js/2.9b887dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a296adaa4a86eabafc4",
+    "url": "/To-do-List-App/static/js/main.00397422.chunk.js"
+  },
+  {
+    "revision": "41a6e2ad2c7dec832edf",
+    "url": "/To-do-List-App/static/js/runtime-main.5f5a567a.js"
+  }
+]);
